3c65752d0585f58d21350e899be5f7a7bae4d117
DESKTOP-T4G2K4O\user <apxkf9@gmail.com>
2024-10-22 11:38:02 +0900

    init

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="logo" class="logo" src="./assets/electron.svg" />
+  <div class="creator">Powered by electron-vite</div>
+  <div class="text">
+    Build an Electron app with
+    <span class="vue">Vue</span>
+  </div>
+  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
+  <div class="actions">
+    <!-- <div class="action">
+      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
+    </div>
+    <div class="action">
+      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
+    </div> -->
+    <div class="action">
+      <a style="color: #42d392;" onmouseout="this.style.color='#42d392';" target="_blank" rel="noreferrer" @click="">자동 경매올리고 포스팅</a>
+    </div>
+
+  </div>
+  <Versions />
+</template>
+
+<script setup>
+import Versions from './components/Versions.vue'
+
+const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+
+</script>
---
+++ src/renderer/src/components/Versions.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="versions">
+    <li class="build">Build v{{ appVersion }}</li>
+    <li class="electron-version">Electron v{{ versions.electron }}</li>
+    <li class="chrome-version">Chrom v{{ versions.chrome }}</li>
+    <li class="node-version">Node v{{ versions.node }}</li>
+  </ul>
+</template>
+
+<script setup>
+import { ref, reactive, onMounted } from 'vue'
+
+const versions = reactive({ ...window.electron.process.versions }) // const versions = ref({ ...window.electron.process.versions })
+const appVersion = ref();
+
+onMounted(async () => {
+  appVersion.value = await window.electron.ipcRenderer.invoke('getAppVersion'); // package.json의 애플리케이션 버전
+  await waitForTimeout(2000);
+
+  // 1. 업데이트 확인
+  await checkUpdate();
+})
+
+const checkUpdate = async () => {
+  window.electron.ipcRenderer.send('checkUpdate');
+}
+
+// 지연시간 추가 setTimeOut 대체
+const waitForTimeout = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+</script>
